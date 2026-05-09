040e4a68776d89b95571042fc2291305f0e7bbb1
Ramanuj Tiwari <tiwariramanujk@gmail.com>
2026-05-09 12:46:26 +0530

    Creation of Middleware

---
+++ Backend/src/middleware/UserMiddleware.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+const userMiddleware = async (req,res,next)=>{
+    try{
+        const {token} = req.cookies;
+        if(!token)
+            throw new Error("Token is not present")
+        const payload = jwt.verify(token,process.env.JWT_TOKEN);
+
+        const{_id} = payload;
+        if(!_id)
+            throw new Error("Id is missing");
+
+        const result = await User.findById(_id);
+        if(!result)
+            throw new Error("User Doesn't Exist");
+
+        next();
+    }
+    catch(err)
+    {
+        res.status(401).send("Error "+err);
+    }
+}
+
+module.exports = userMiddleware;
