f3357e99623f588a0a80b98f9698de365c0c738a
Ramanuj Tiwari <tiwariramanujk@gmail.com>
2026-05-18 09:51:52 +0530

    Axios is created and the routing for login and signup page is done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,17 @@
+import { Routes,Route,Navigate } from "react-router";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
 function App() {
   return (
-    <div className="flex flex-col justify-center items-center bg-blue-400">
-      "Hello Everyone"
-    </div>
+    <>
+    <Routes>
+      <Route path ="/" element={<Home/>}/>
+      <Route path ="/signup" element={<Signup/>}/>
+      <Route path ="/login" element={<Login/>}/>
+    </Routes>
+    </>
   )
 }
 
